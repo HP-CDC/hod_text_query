74964a107178b020b7c232f1bd93ce8119ea34f8
tylernappy <tylernappy@gmail.com>
2015-10-12 13:09:21 -0400

    added button and attached to socket

--- index.js
+++ index.js
@@ -20,23 +20,38 @@ var twitterClient = new Twitter({
 port = process.env.PORT || 5000
 app.use(express.static(path.join(__dirname, 'public')));
 
-twitterClient.stream('statuses/filter', {track: 'coffee'}, function(stream) {
-  stream.on('data', function(tweet) {
-    var data = {text: tweet.text}
-    hodClient.call('analyzesentiment', data, function(err, resp){
-      var sentiment = resp.body.aggregate.sentiment
-      var score = resp.body.aggregate.score
-      console.log("------------------------------")
-      console.log(tweet.text + " | " + sentiment + " | " + score)
-      var tweetData = {text: tweet.text, positive: resp.body.positive, negative: resp.body.negative, aggregate: resp.body.aggregate}
-      io.emit('tweetData', tweetData)
-    })
+// io.on('streamTopic', function(msg){
+//   console.log(msg)
+// })
+
+io.on('connection', function(socket){
+
+  // socket.on('streamTopic', function(msg){
+  //   console.log(msg)
+  // })
+
+  socket.on('streamTopic', function(msg){
+    console.log(msg);
+    twitterClient.stream('statuses/filter', {track: msg.topic}, function(stream) {
+      stream.on('data', function(tweet) {
+        var data = {text: tweet.text}
+        hodClient.call('analyzesentiment', data, function(err, resp){
+          var sentiment = resp.body.aggregate.sentiment
+          var score = resp.body.aggregate.score
+          console.log("------------------------------")
+          console.log(tweet.text + " | " + sentiment + " | " + score)
+          var tweetData = {text: tweet.text, positive: resp.body.positive, negative: resp.body.negative, aggregate: resp.body.aggregate}
+          io.emit('tweetData', tweetData)
+        })
+      });
+
+      stream.on('error', function(error) {
+        throw error;
+      });
+    });
   });
 
-  stream.on('error', function(error) {
-    throw error;
-  });
-});
+})
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + '/views/index.html');
