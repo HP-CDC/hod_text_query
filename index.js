36d3a6dc1907380d2d1f1a453d6894c60cce3211
tylernappy <tylernappy@gmail.com>
2015-10-12 10:33:55 -0400

    added twitter client library

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
+require('dotenv').load()
+
 var express = require('express')
 var app = express()
 var http = require('http').Server(app)
 var path = require('path')
 var io = require('socket.io')(http)
+var Twitter = require('twitter')
+
+var twitterClient = new Twitter({
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token_key: process.env.access_token,
+  access_token_secret: process.env.access_token_secret
+});
 
 port = process.env.PORT || 5000
 app.use(express.static(path.join(__dirname, 'public')));
+
+twitterClient.stream('statuses/filter', {track: 'coffee'}, function(stream) {
+  stream.on('data', function(tweet) {
+    console.log("------------------------------");
+    console.log(tweet.text);
+  });
+
+  stream.on('error', function(error) {
+    throw error;
+  });
+});
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + '/views/index.html');
