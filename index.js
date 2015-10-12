a0c03e02298334ef085672ce80ead91e74dccca4
tylernappy <tylernappy@gmail.com>
2015-10-12 11:21:43 -0400

    added HOD

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var http = require('http').Server(app)
 var path = require('path')
 var io = require('socket.io')(http)
 var Twitter = require('twitter')
+var havenondemand = require('havenondemand')
+
+var hodClient = new havenondemand.HODClient('http://api.havenondemand.com', process.env.hpe_apikey)
 
 var twitterClient = new Twitter({
   consumer_key: process.env.consumer_key,
@@ -19,8 +22,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 twitterClient.stream('statuses/filter', {track: 'coffee'}, function(stream) {
   stream.on('data', function(tweet) {
-    console.log("------------------------------");
-    console.log(tweet.text);
+    var data = {text: tweet.text}
+    hodClient.call('analyzesentiment', data, function(err, resp){
+      var sentiment = resp.body.aggregate.sentiment
+      var score = resp.body.aggregate.score
+      console.log("------------------------------")
+      console.log(tweet.text + " | " + sentiment + " | " + score)
+    })
   });
 
   stream.on('error', function(error) {
