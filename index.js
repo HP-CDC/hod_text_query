a8890cc533c1c913c3cd2606ac832794f1f1184c
tylernappy <tylernappy@gmail.com>
2015-10-13 10:09:29 -0400

    added Profile; fixed layout

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-require('dotenv').load()
+require('dotenv').load();
 
-var express = require('express')
-var app = express()
-var http = require('http').Server(app)
-var path = require('path')
-var io = require('socket.io')(http)
-var Twitter = require('twitter')
-var havenondemand = require('havenondemand')
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var path = require('path');
+var io = require('socket.io')(http);
+var Twitter = require('twitter');
+var havenondemand = require('havenondemand');
 
-var hodClient = new havenondemand.HODClient('http://api.havenondemand.com', process.env.hpe_apikey)
+var hodClient = new havenondemand.HODClient('http://api.havenondemand.com', process.env.hpe_apikey);
 
 var twitterClient = new Twitter({
   consumer_key: process.env.consumer_key,
@@ -17,8 +17,12 @@ var twitterClient = new Twitter({
   access_token_secret: process.env.access_token_secret
 });
 
-port = process.env.PORT || 5000
+port = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, 'public')));
+
+newAverage = 0;
+oldAverage = 0;
+n = 0;
 
 // io.on('streamTopic', function(msg){
 //   console.log(msg)
@@ -26,23 +30,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', function(socket){
 
-  // socket.on('streamTopic', function(msg){
-  //   console.log(msg)
-  // })
+  socket.on('disconnect', function () {
+    socket.emit('user disconnected');
+  });
 
   socket.on('streamTopic', function(msg){
+    newAverage = 0;
+    oldAverage = 0;
+    n = 0;
     console.log(msg);
     twitterClient.stream('statuses/filter', {track: msg.topic}, function(stream) {
       stream.on('data', function(tweet) {
-        var data = {text: tweet.text}
+        var data = {text: tweet.text};
         hodClient.call('analyzesentiment', data, function(err, resp){
-          var sentiment = resp.body.aggregate.sentiment
-          var score = resp.body.aggregate.score
-          console.log("------------------------------")
-          console.log(tweet.text + " | " + sentiment + " | " + score)
-          var tweetData = {text: tweet.text, positive: resp.body.positive, negative: resp.body.negative, aggregate: resp.body.aggregate}
-          io.emit('tweetData', tweetData)
-        })
+          if (!err) {
+            if (resp.body.aggregate !== undefined) {
+              n += 1; //increase n by one
+              var sentiment = resp.body.aggregate.sentiment;
+              var score = resp.body.aggregate.score;
+              newAverage = calculateRunningAverage(score, n);
+              rgbInstantaneous = mapColor(score);
+              rgbAverage = mapColor(newAverage);
+              console.log("------------------------------");
+              console.log(tweet.text + " | " + sentiment + " | " + score);
+              var tweetData = {tweet: tweet, positive: resp.body.positive, negative: resp.body.negative, aggregate: resp.body.aggregate, rgbInstantaneous: rgbInstantaneous, rgbAverage: rgbAverage, average: newAverage};
+              io.emit('tweetData', tweetData);
+            }
+          }
+        });
+      });
+
+      stream.on('disconnect', function (disconnectMessage) {
+        console.log(disconnectMessage);
       });
 
       stream.on('error', function(error) {
@@ -51,12 +70,26 @@ io.on('connection', function(socket){
     });
   });
 
-})
+});
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + '/views/index.html');
-})
+});
 
 http.listen(port, function(){
-  console.log("Listening on port: "+port)
-})
+  console.log("Listening on port: "+port);
+});
+
+mapColor = function (score) {
+  weight = Math.floor(((0.5*score + 0.5)*100));
+  r = Math.floor( (255 * (100 - weight)) / 100 );
+  g = Math.floor( (255 * weight) / 100 );
+  b = 0;
+  return {r: r, g: g, b:b};
+}
+
+calculateRunningAverage = function(score, n) {
+  newAverage = oldAverage * (n-1)/n + score/n;   // New average = old average * (n-1)/n + new value /n
+  oldAverage = newAverage; //set equal to new average for next go around of calling this function
+  return newAverage;
+}
